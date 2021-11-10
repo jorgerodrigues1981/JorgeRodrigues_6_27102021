d93b5f86f8d5ec5d64454adebe6090e45c113f8d
jorgerodrigues1981 <84859915+jorgerodrigues1981@users.noreply.github.com>
2021-11-10 14:58:27 +0100

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,10 +1,14 @@
+// Package jsonwebtoken pour attribuer un token à un utilisateur 
 const jwt = require('jsonwebtoken');
 
-//Package pour pouvoir créer et vérifier les tokens d'authentification
+// Middleware appliqué à toutes les routes afin de les sécuriser
 module.exports = (req, res, next) => {
   try {
+    // On récupère le token dans le header de la requête autorisation, on récupère uniquement le deuxième élément du tableau (car split)
     const token = req.headers.authorization.split(' ')[1];
+    // On vérifie le token décodé avec la clé secrète initiéé avec la création du token encodé initialement
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    // On vérifie que le userId envoyé avec la requête correspond au userId encodé dans le token
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
